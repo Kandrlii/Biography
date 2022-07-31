a795455feb0c4f0d138f55723e13aa8a7fe67187
Kandrlii <108890171+Kandrlii@users.noreply.github.com>
2022-07-31 18:24:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-for (;;) {
-let nationality = prompt("Підтвердіть, що ви українець (Напишіть: Слава Україні): ")
-    if (nationality == "Слава Україні") {
-        alert("Доступ дозволено!")
-        break
-    } else {
-        alert("Доступ заборонено, ви не українець!")
-    }
-}
-
 function AboutMe() {
     alert("Instagram: @couchytry\nTelegram: @quietnesse")
 }
